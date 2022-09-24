fb7575a2e507d4b6dd6b80ae8cc2dcd1a396970a
sh89e <sh89e@DESKTOP-K74CSHN>
2022-09-24 05:05:32 -0700

    Add project files.

---
+++ ProductionCap/ProductionCapClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+
+namespace ProductionCap
+{
+    public class ProductionCapClass : MelonMod
+    {
+        static int i = 0;
+        public override void OnUpdate()
+        {
+            GameObject go = GameObject.FindGameObjectWithTag("GameManager");
+            if (go != null)
+            {
+                GameManager gameManager = go.GetComponent<GameManager>();
+
+                if (gameManager != null)
+                {
+                    if (i % 100 != 0)
+                    {
+                        i++;
+                        base.OnUpdate();
+                        return;
+                    }
+                    if (gameManager.resourceManager.potteryItemInfo.unusedCount > 175)
+                    {
+                        Il2CppSystem.Collections.Generic.List<PotterBuilding> potterBuildings = gameManager.resourceManager.potterBuildings;
+                        MelonLogger.Msg(potterBuildings.ToString());
+                        foreach (PotterBuilding potterBuilding in potterBuildings)
+                        {
+                            potterBuilding.isWorkEnabled = false;
+                            potterBuilding.enabled = false;
+                            MelonLogger.Msg("Potter building disalbed");
+                        }
+                    }
+                    else
+                    {
+                        Il2CppSystem.Collections.Generic.List<PotterBuilding> potterBuildings = gameManager.resourceManager.potterBuildings;
+                        foreach (PotterBuilding potterBuilding in potterBuildings)
+                        {
+                            potterBuilding.isWorkEnabled = true;
+                            potterBuilding.enabled = true;
+                            MelonLogger.Msg("Potter building enabled");
+                        }
+                    }
+
+                }
+            }
+
+            base.OnUpdate();
+        }
+    }
+}
---
+++ ProductionCap/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using ProductionCap;
+
+[assembly: MelonInfo(typeof(ProductionCapClass),"Production Cap", "0.0.1", "sh89elf")]
+[assembly: MelonGame("Crate Entertainment", "Farthest Frontier")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProductionCap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductionCap")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25abecc9-5af2-45c6-ac07-c4f0046b19ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
